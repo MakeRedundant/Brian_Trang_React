bb40ac829a7e5b9899d1bb98b803b18a6bb8fc41
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-04 15:54:49 +1100

    added About, contact, edu, exp, footer, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import React from "react";
+import About from './components/About/About';
+import Experience from './components/About/About';
+import About from './components/About/About';
+import About from './components/About/About';
+import About from './components/About/About';
 import About from './components/About/About';
 
 const App = () => {
   return (
     <>
       <About />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Contact />
     </>
   );
 };
