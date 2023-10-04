ee281d9413d65a99584f610f80e45270a334e613
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-04 16:24:06 +1100

    added public javascript files

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-
-import './App.css'
+import "./App.css";
 import React from "react";
-import Header from './components/Header/Header';
-import About from './components/About/About';
-import Experience from './components/Experience/Experience';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Project/Projects';
-import Education from './components/Education/Education';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-
+import Header from "./components/Header/Header";
+// import About from './components/About/About';
+// import Experience from './components/Experience/Experience';
+// import Skills from './components/Skills/Skills';
+// import Projects from './components/Projects/Projects';
+// import Education from './components/Education/Education';
+// import Contact from './components/Contact/Contact';
+// import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
-      <About />
+      {/* <About />
       <Experience />
       <Skills />
       <Projects />
       <Education />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <div
+        className="container"
+        style={{
+          background:
+            "url('assets/astronaut-man.jpg') no-repeat center center/cover",
+        }}
+      >
+        <div className="zero-section">
+          <div className="text-box">
+            <h2 id="hello" onClick={changeLanguage}>
+              👋 Hello{" "}
+            </h2>
+            <p>
+              My name is Brian, and I'm a software engineer <br />
+              based in Australia Sydney 🦘{" "}
+            </p>
+            <p>I like to eat good food 👔 and wear good clothes 🍔</p>
+            <br />
+            <div id="Codingtext"></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
