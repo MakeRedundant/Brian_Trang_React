e78fada74d601fb015a567449a1da77de7af61ff
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-04 22:55:06 +1100

    updated education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import "./Education.css";
-import USYDLogo from '../../assets/USYD.png';
-import UNSWLogo from '../../assets/UNSW.png';
+import USYDLogo from "../../assets/USYD.png";
+import UNSWLogo from "../../assets/UNSW.png";
 
 const Education = () => {
   return (
