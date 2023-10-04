e08df773fb985240bf5c791882af3692346f9be0
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-04 17:14:07 +1100

    updated eslint

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,28 @@
-import "./nav.css";
+import './nav.css';
 
 const NavBar = () => {
   return (
     <nav>
       <a href="#zero" data-text="Home">
-        <i class="far fa fa-home"></i>
+        <i className="far fa fa-home"></i>
       </a>
       <a href="#first" data-text="About">
-        <i class="far fa-user"></i>
+        <i className="far fa-user"></i>
       </a>
       <a href="#second" data-text="Experience">
-        <i class="fas fa-briefcase"></i>
+        <i className="fas fa-briefcase"></i>
       </a>
       <a href="#third" data-text="Skills">
-        <i class="icon fa fa-code"></i>
+        <i className="icon fa fa-code"></i>
       </a>
       <a href="#fourth" data-text="Project">
-        <i class="far fa-folder-open"></i>
+        <i className="far fa-folder-open"></i>
       </a>
       <a href="#fifth" data-text="Education">
-        <i class="far fa fa-school"></i>
+        <i className="far fa fa-school"></i>
       </a>
       <a href="#sixth" data-text="Contact">
-        <i class="far fa fa-phone"></i>
+        <i className="far fa fa-phone"></i>
       </a>
     </nav>
   );
