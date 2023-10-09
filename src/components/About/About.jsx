2e358c28e7e6459800947cbc4e5764c2b0607d93
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-09 12:23:51 +1100

    added typewriter effect to About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,12 @@
 import "./About.css";
+import { TypewriterComponent2 } from "../utils/Typewriter/Typewriter2";
 // import GlsPhoto from '../../assets/Paedriatic-Gls.jpg';
 // import ShakingEyesGif from '../../assets/capoo-gaming.gif';
 
 const About = () => {
   return (
     <div className="first-section">
-      <section id='about'>
+      <section id="about">
         <h1>About</h1>
         <div className="container">
           <div className="shaking-eyes-gif">
@@ -13,13 +14,19 @@ const About = () => {
           </div>
           <div className="about-text">
             <div className="text-box">
-              <p> As a optometrist i worked in a wide range of fields including
-                Paedriatics, Refractive (LASIK), Glaucoma, Orthoptic and Retinal <br/>
-                 And after more than 5 years as an Optometrist,
-                I have decided to jump over to programming! <br />
-                When I&apos;m not coding, you can find me watching
-                YouTube coding tutorials or cat videos 🐱‍💻.
-              <br />
+              <p>
+                {" "}
+                As a optometrist i worked in a wide range of fields including
+                Paedriatics, Refractive (LASIK), Glaucoma, Orthoptic and Retinal{" "}
+                <br />
+                And after more than 5 years as an Optometrist, I have decided to
+                jump over to programming! <br />
+                <p>
+                I also like <TypewriterComponent2 />
+                </p>
+                When I&apos;m not coding, you can find me watching YouTube
+                coding tutorials or cat videos 🐱‍💻.
+                <br />
                 Look around, and I hope to hear back from you!
               </p>
             </div>
---
+++ src/components/utils/Typewriter/Typewriter2.jsx
@@ -0,0 +1,13 @@
+import { Typewriter } from "react-simple-typewriter";
+
+export const TypewriterComponent2 = ({ words, loop, typeSpeed, deleteSpeed, delaySpeed }) => {
+  return (
+    <Typewriter
+      words={["Web Development", "Javascript", "UI/UX", "CSS", "React", "Vue", "Fancy Websites"]}
+      loop={true}
+      typeSpeed={50}
+      deleteSpeed={10}
+      delaySpeed={2000}
+    />
+  );
+};
