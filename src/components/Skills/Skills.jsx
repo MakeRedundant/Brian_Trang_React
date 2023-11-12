a2d60e730c506fe642a44dfd69cf49ef8e882164
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-11-12 18:12:05 +1100

    updated experience, added more skills (devicon)

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,7 +3,51 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <section id= "skills" className="tools">
-      <h2 className="tools__titulo titulo__dark">Skills</h2>
+      <h1> Skills</h1>
+      <div className="user-skills">
+        <div className="tech">
+          <h2>Languages</h2>
+          <i className="devicon-javascript-plain colored"></i>
+          <i className="devicon-typescript-plain colored"></i>
+          <i className="devicon-html5-plain-wordmark colored"></i>
+          <i className="devicon-css3-plain-wordmark colored"></i>
+          <p>My favorite language for programming</p>
+        </div>
+        <div className="tech">
+          <h2>Front-End</h2>
+          <i className="devicon-react-original colored"></i>
+          <i className="devicon-vuejs-plain"></i>
+          <i className="devicon-svelte-plain colored"></i>
+          <i className="devicon-bootstrap-plain-wordmark colored"></i>
+          <i className="devicon-tailwindcss-plain colored"></i>
+          <i className="devicon-handlebars-plain colored"></i>
+          <p>My preferred technologies for front-end web development and component design.</p>
+        </div>
+        <div className="tech">
+          <h2>Back-End</h2>
+          <i className="devicon-nodejs-plain colored"></i>
+          <i className="devicon-express-original-wordmark"></i>          <i className="devicon-postgresql-plain-wordmark colored"></i>
+          <i className="devicon-mysql-plain-wordmark"></i>
+          <i className="devicon-mongodb-plain-wordmark colored"></i>
+          <i className="devicon-graphql-plain colored"></i>
+          <p>My preferred technologies for back-end web programming and database architecture.</p>
+        </div>
+        <div className="tech">
+          <h2>Tools</h2>
+          <i className="devicon-git-plain-wordmark colored"></i>
+          <i className="devicon-visualstudio-plain colored"></i>
+          <i className="devicon-github-plain-wordmark colored"></i>
+          <i className="devicon-docker-plain-wordmark colored"></i>
+          <i className="devicon-slack-plain colored"></i>
+          <i className="devicon-gitlab-plain colored"></i>
+          <i className="devicon-figma-plain colored"></i>
+          <i className="devicon-docker-plain colored"></i>
+          <i className="devicon-bash-plain"></i>
+          <i className="devicon-codepen-plain"></i>
+          <p>My preferred tools for version control, code editing, and container orchestration.</p>
+        </div>
+      </div>
+      <h2>My Tools</h2>
       <div className="tools__caja__slider">
         <div className="slider">
           <div className="tools__cajas">
