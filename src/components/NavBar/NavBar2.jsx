87757ab2b0b3eac92501a8ff54460d985b8c8681
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-12-18 22:17:24 +1100

    improve accessibility

--- src/components/NavBar/NavBar2.jsx
+++ src/components/NavBar/NavBar2.jsx
@@ -5,31 +5,31 @@ const NavBar2 = () => {
     <nav className="navbar2">
       {/* Second Navigation Bar */}
       <div className="navbar2-container">
-        <a href="#header">
+        <a href="#header" data-text="Home">
           <i className="far fa fa-home"></i>
           Home
         </a>
-        <a href="#about">
+        <a href="#about" data-text="About">
           <i className="far fa-user"></i>
           About
         </a>
-        <a href="#experience">
+        <a href="#experience" data-text="Experience">
           <i className="fas fa-briefcase"></i>
           Experience
         </a>
-        <a href="#skills">
+        <a href="#skills" data-text="Skills">
           <i className="far fa-file-code"></i>
           Skills
         </a>
-        <a href="#project">
+        <a href="#project" data-text="Project">
           <i className="far fa-folder-open"></i>
           Project
         </a>
-        <a href="#education">
+        <a href="#education" data-text="Education">
           <i className="far fa fa-school"></i>
           Education
         </a>
-        <a href="#contact">
+        <a href="#contact" data-text="Contact">
           <i className="far fa fa-phone"></i>
           Contact || Resume
         </a>
