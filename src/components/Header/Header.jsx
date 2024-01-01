4c2a1d8b57cf095a6250cd31daed19df0d98b3fc
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-01 20:25:22 +1100

    preloading MeSlit image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
         </div>
 
         <div className="header-image">
-          <img src={ME} alt="Me" />
+          <img src={ME} alt="Me" width="70%" height="100%" />
         </div>
       </div>
     </header>
