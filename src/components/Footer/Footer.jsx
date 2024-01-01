f5d66e72d9a4831e7c15d7e9ba407f29d14c0a81
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-01 22:28:06 +1100

    fixed links without discernible desc

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,13 @@ const FooterBottom = () => {
       <p>Made by Brian Trang</p>
       <div className="social-icons">
         <a href="https://github.com/MakeRedundant">
-          <FontAwesomeIcon icon={faGithub} size="2x" className="icon" />
+          <FontAwesomeIcon icon={faGithub} size="2x" className="icon" alt="Github" />
         </a>
         <a href="https://www.linkedin.com/in/brian-t-webdeveloper/">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" />
+          <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" alt="Linkedin" />
         </a>
         <a href="https://twitter.com/fireship_dev">
-          <FontAwesomeIcon icon={faTwitter} size="2x" className="icon" />
+          <FontAwesomeIcon icon={faTwitter} size="2x" className="icon" alt="Twitter" />
         </a>
       </div>
     </div>
