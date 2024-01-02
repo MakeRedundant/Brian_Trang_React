84aea6ac5801a74a29852dbf417c803ffcc1c1cb
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2024-01-02 14:57:47 +1100

    fixed links without discernible desc

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,14 +11,18 @@ const FooterBottom = () => {
     <div className="footer-text">
       <p>Made by Brian Trang</p>
       <div className="social-icons">
-        <a href="https://github.com/MakeRedundant">
-          <FontAwesomeIcon icon={faGithub} size="2x" className="icon" alt="Github" />
+        <a href="https://github.com/MakeRedundant" 
+          aria-label="Link to Github">
+          <FontAwesomeIcon icon={faGithub} size="2x" className="icon" />
         </a>
-        <a href="https://www.linkedin.com/in/brian-t-webdeveloper/">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" alt="Linkedin" />
+        <a
+          href="https://www.linkedin.com/in/brian-t-webdeveloper/"
+          aria-label="Link to LinkedIn"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" />
         </a>
-        <a href="https://twitter.com/fireship_dev">
-          <FontAwesomeIcon icon={faTwitter} size="2x" className="icon" alt="Twitter" />
+        <a href="https://twitter.com/fireship_dev" aria-label="Link to Twitter">
+          <FontAwesomeIcon icon={faTwitter} size="2x" className="icon" />
         </a>
       </div>
     </div>
